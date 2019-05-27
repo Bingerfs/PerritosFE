2141e00beed0c8d21be6208374462f59192a4334
Julia1297 <valita129@gmail.com>
2019-05-27 11:51:58 -0400

    Búsqueda

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink, Router } from '@angular/router';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'prac3fe-home',
@@ -7,11 +8,25 @@ import { RouterLink, Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public rows;
 
-  constructor(private router: Router) { }
+  constructor(public apiService: ApiService , public router : Router) { }
 
   ngOnInit() {
   }
   onClikMe(){
+  }
+  public buscar(){
+    let cadena = document.getElementById("search") as HTMLInputElement;
+    console.log("buscando : " + cadena.value);
+    var path = 'google_custom_search/index?parametro=' + cadena;
+    this.apiService.get(path).subscribe((data )=>{
+      console.log(data);
+      this.rows = data;
+      });
+      // this.apiService.get("perritos/"+data.id).subscribe((data : Perritos)=>{
+      //   this.perrito = data;
+      //   });
+
   }
 }
